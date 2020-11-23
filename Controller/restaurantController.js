26465e21ecc1ab25f5d996ed38698d6232e55d7c
QamarJaradat <moon19.qs@gmail.com>
2020-11-23 15:51:01 +0200

    Restaurant DB + FeaadBack Controller

--- Controller/restaurantController.js
+++ Controller/restaurantController.js
@@ -1,14 +1,15 @@
 const Restaurant = require('../DataModel').resturants
+const restData = require('../restdata/restaurant.json')
 
 
-exports.loginUser = (req, res) => {
-
-
-
-
-
-    
+exports.fillrest = (req, res) => {
+    for (let index = 0; index < restData.length; index++) {
+        var rest = new Restaurant(restData[index])
+        rest.save((err, rest1) => {
+            if (err)
+                console.log(err)
+            console.log(rest1)
+        })
+    }
+    res.send(restData)
 }
-
-
-module.exports = RestaurantController
--- routes.js
+++ routes.js
@@ -2,13 +2,20 @@ const express = require('express');
 const routers = express.Router();
 const auth = require('./auth')
 //User Controller 
-// const userController = require('./Controller/UserController')
-// routers.post('/signup', userController.signUpUser);
-// routers.post('/login', userController.loginUser);
-// routers.post('/logout', userController.userlogout)
-// routers.get('/checkuser', auth, (req, res) => {
-//     res.send(userController.checkuser(req, res))
-// })
-// routers.post('/getuserinfo', userController.getuserinfo)
+const userController = require('./Controller/UserController')
+routers.post('/signup', userController.signUpUser);
+routers.post('/login', userController.loginUser);
+routers.post('/logout', userController.userlogout)
+routers.get('/checkuser', auth, (req, res) => {
+    res.send(userController.checkuser(req, res))
+})
+routers.post('/getuserinfo', userController.getuserinfo)
+
+//Restaurant Controller
+const RestaurantController = require('./Controller/restaurantController')
+
+routers.post('/filldata', RestaurantController.fillrest)
+
+
 
 module.exports = routers;
