c76ec81c12d55d6f271208e55adbcafea182b42b
imadaqel <emadaqel92@gmail.com>
2020-11-23 11:31:11 +0200

    db

--- Controller/restaurantController.js
+++ Controller/restaurantController.js
@@ -1,27 +1,13 @@
-const Restaurant = require('../DataModel').trips
+const Restaurant = require('../DataModel').resturants
 
 
-let RestaurantController =
-{
-    find: async (req, res) => {
-        let found = await Restaurant.find({ name: req.params.Name });
-        res.json(found);
-    },
-    all: async (req, res) => {
-        let allRestaurants = await Restaurant.find();
-        res.json(allRestaurants);
-    },
-    create: async (req, res) => {
-        let newRestaurant = new Restaurant(req.body);
-        let saveRestaurant = await newRestaurant.save();
-        res.json(saveRestaurant);
-    },
-    getAllRestaurant: async (req, res) => {
-        let foundRestaurant = await Restaurant.find({ name: req.params.Name }).populate('category');
-        res.json(foundRestaurant)
-    }
+exports.loginUser = (req, res) => {
 
 
+
+
+
+    
 }
 
 
